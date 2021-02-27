ed03e343b53cf0afa91d88d383c8e1d521ea7dac
MH4454 <68453774+MH4454@users.noreply.github.com>
2021-02-26 21:56:42 -0500

    Added DB post transaction logic

--- Develop/public/indexed-DB.js
+++ Develop/public/indexed-DB.js
@@ -1,13 +1,52 @@
 const DB_NAME = 'BudgetAppDB';
 const DB_VERSION = 1;
 const DB_STORE_NAME = 'BudgetAppStatement';
-
+console.log("hello");
 var db;
 // Create Database with version #
 var request = indexedDB.open(DB_NAME, DB_VERSION);
 request.onerror = function(event) {
-  console.log("Why didn't you allow my web app to use IndexedDB?!");
+  console.log(`Error: ${event.target.error} was found!`);
 };
+
+request.onupgradeneeded = function(event) {
+  db = event.target.result
+  // Create Obj Store with autoIncrement which gives the store a key generator
+  db.createObjectStore(DB_STORE_NAME,{ autoIncrement: true})
+}
+
 request.onsuccess = function(event) {
   db = event.target.result;
+  openTransaction()
+  if (navigator.onLine){
+    postRecords()
+  } else {
+    saveRecords()
+  }
 };
+
+function openTransaction() {
+  // Tells IndexedDB to allow read/write access to the DB
+  const transaction = db.transaction([DB_NAME],"readwrite");
+  // Tells IndexedDB to access the object store to start transaction
+  const dataStore = transaction.objectStore(DB_STORE_NAME)
+
+  return dataStore
+}
+
+function postRecords() {
+  const records = dataStore.getAll();
+  records.onsuccess = () => {
+    if (records.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(getAll.result)
+      })
+      .then((response) => response.json())
+      .then(() => {
+       openTransaction().clear()
+      });
+  }}}
